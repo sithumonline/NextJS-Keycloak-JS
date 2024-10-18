30ae689fcdfb60beab6d1b416bc3590b72e2e442
Sithum Bopitiya <sithumsandeepap@gmail.com>
2024-10-18 08:01:46 +0530

    Send request to backend

--- src/components/Dashboard/DashboardHabits.jsx
+++ src/components/Dashboard/DashboardHabits.jsx
@@ -1,7 +1,33 @@
 import WeeklyHabitAny from "../Habits/WeeklyHabitAny";
 import WeeklyHabitFixed from "../Habits/WeeklyHabitFixed";
 
+import Cookies from "js-cookie";
+
 export default function DashboardHabits() {
+    const sendRequest = async () => {
+        // get keycloak-token from cookie
+        const token = Cookies.get("keycloak-token");
+        // set headers with token
+        const headers = {
+            Authorization: `Bearer ${token}`,
+        };
+
+        // send GET request to the server
+        const response = await fetch("http://localhost:3002/api/test-auth", {
+            method: "GET",
+            headers,
+        });
+
+        // if response is not ok, throw an error
+        if (!response.ok) {
+            console.error("Failed to fetch the authenticated message.");
+            return;
+        }
+
+        const res = await response.json();
+        console.log("Response from server:", res);
+    }
+
     return (
         <section className="w-4/5 mx-auto flex flex-col gap-8">
             <div className="flex justify-between items-center">
@@ -10,7 +36,8 @@ export default function DashboardHabits() {
                 <h4 className="text-xl uppercase [word-spacing:3px]">Week 26 June 24-30</h4>
             </div>
             <WeeklyHabitFixed habitTitle="Walk the dog"/>
-            <WeeklyHabitAny habitTitle="Take out the trash"/>
+            <WeeklyHabitAny habitTitle="Take out the trash" />
+            <button onClick={sendRequest} className="bg-purple text-white uppercase py-2 px-4 rounded-md">Send Request</button>
         </section>
     )
 }
