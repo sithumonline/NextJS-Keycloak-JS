d9bff53ac82982ccaaf11c21732df1383003ca90
Sithum Bopitiya <sithumsandeepap@gmail.com>
2024-10-18 08:01:12 +0530

    Add a button for login

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,17 +1,17 @@
-import { useEffect, useState } from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // Updated import
-import Keycloak from 'keycloak-js';
-import Cookies from 'js-cookie';
-import MainComp from '.';
-import Logout from './logout';
-import About from './about';
+import { useEffect, useState } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom"; // Updated import
+import Keycloak from "keycloak-js";
+import Cookies from "js-cookie";
+import MainComp from ".";
+import Logout from "./logout";
+import About from "./about";
 
 function App() {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
   const [userProfile, setUserProfile] = useState(null);
 
   useEffect(() => {
-    const isAlreadyAuthenticated = Cookies.get('isAuthenticated');
+    const isAlreadyAuthenticated = Cookies.get("isAuthenticated");
 
     if (isAlreadyAuthenticated) {
       setIsAuthenticated(true);
@@ -21,31 +21,82 @@ function App() {
         realm: process.env.NEXT_PUBLIC_KEYCLOAK_REALM,
         clientId: process.env.NEXT_PUBLIC_KEYCLOAK_CLIENT_ID,
       };
+
       const keycloak = new Keycloak(keycloakConfig);
 
-      keycloak.init({ onLoad: 'login-required' })
-        .then(authenticated => {
+      // keycloak.init({ onLoad: 'login-required' })
+      keycloak
+        .init()
+        .then((authenticated) => {
           setIsAuthenticated(authenticated);
           if (authenticated) {
-            Cookies.set('isAuthenticated', 'true', { expires: 1 });
-            Cookies.set('keycloak-token', keycloak.token, { expires: 1 });
-            Cookies.set('keycloak-refresh-token', keycloak.refreshToken, { expires: 1 });
-            keycloak.loadUserProfile().then(profile => {
-              setUserProfile(profile);
-            }).catch(error => {
-              console.error("Failed to load user profile", error);
+            Cookies.set("isAuthenticated", "true", { expires: 1 });
+            Cookies.set("keycloak-token", keycloak.token, { expires: 1 });
+            Cookies.set("keycloak-refresh-token", keycloak.refreshToken, {
+              expires: 1,
             });
+            keycloak
+              .loadUserProfile()
+              .then((profile) => {
+                setUserProfile(profile);
+              })
+              .catch((error) => {
+                console.error("Failed to load user profile", error);
+              });
           }
         })
-        .catch(error => {
+        .catch((error) => {
           console.error("Initialization failed", error);
         });
     }
   }, []);
 
- 
+  const handleLogin = () => {
+    const keycloakConfig = {
+      url: process.env.NEXT_PUBLIC_KEYCLOAK_URL,
+      realm: process.env.NEXT_PUBLIC_KEYCLOAK_REALM,
+      clientId: process.env.NEXT_PUBLIC_KEYCLOAK_CLIENT_ID,
+    };
+
+    const keycloak = new Keycloak(keycloakConfig);
+
+    keycloak
+      .init({ onLoad: "login-required" })
+      .then((authenticated) => {
+        setIsAuthenticated(authenticated);
+        if (authenticated) {
+          Cookies.set("isAuthenticated", "true", { expires: 1 });
+          Cookies.set("keycloak-token", keycloak.token, { expires: 1 });
+          Cookies.set("keycloak-refresh-token", keycloak.refreshToken, {
+            expires: 1,
+          });
+          keycloak
+            .loadUserProfile()
+            .then((profile) => {
+              setUserProfile(profile);
+            })
+            .catch((error) => {
+              console.error("Failed to load user profile", error);
+            });
+        }
+      })
+      .catch((error) => {
+        console.error("Initialization failed", error);
+      });
+  };
+
   if (!isAuthenticated) {
-    return <div>Loading...</div>;
+    // return <div>Loading...</div>;
+    return (
+      <div>
+        <button
+          onClick={handleLogin}
+          className="bg-purple text-white uppercase py-2 px-4 rounded-md"
+        >
+          Login
+        </button>
+      </div>
+    );
   }
 
   return (
@@ -57,8 +108,7 @@ function App() {
         {/* Other Routes */}
       </Routes>
     </Router>
-);
+  );
 }
 
 export default App;
-
